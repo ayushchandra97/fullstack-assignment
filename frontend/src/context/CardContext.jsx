19b2c989e7bdbabaad0f87c298dfa66b97a01272
ayushchandra97 <ayushchandra097@gmail.com>
2024-08-19 12:12:27 +0530

    Update for consistency

--- frontend/src/context/CardContext.jsx
+++ frontend/src/context/CardContext.jsx
@@ -11,28 +11,28 @@ function CardContextProvider({ children }) {
     // If no cards in the database, sample cards will be displayed.
     const sampleCards = [
         {
-            title: 'Branches',
-            description: 'Abstract Branches lets you manage, version, and document your designs in one place.'
+            title: "Branches",
+            description: "Abstract Branches lets you manage, version, and document your designs in one place."
         },
         {
-            title: 'Manage your account',
-            description: 'Configure your account settings, such as your email, profile details, and password.'
+            title: "Manage your account",
+            description: "Configure your account settings, such as your email, profile details, and password."
         },
         {
-            title: 'Manage organizations, teams, and projects',
-            description: 'Use Abstract organizations, teams, and projects to organize your people and your work.'
+            title: "Manage organizations, teams, and projects",
+            description: "Use Abstract organizations, teams, and projects to organize your people and your work."
         },
         {
-            title: 'Manage billing',
-            description: 'Change subscriptions and payment details.'
+            title: "Manage billing",
+            description: "Change subscriptions and payment details."
         },
         {
-            title: 'Authenticate to Abstract',
-            description: 'Set up and configure SSO, SCIM, and Just-in-Time provisioning.'
+            title: "Authenticate to Abstract",
+            description: "Set up and configure SSO, SCIM, and Just-in-Time provisioning."
         },
         {
-            title: 'Abstract support',
-            description: 'Get in touch with a human.'
+            title: "Abstract support",
+            description: "Get in touch with a human."
         },
     ]
 
